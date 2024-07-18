15dd112abdb8690c64d4846c3401c860e9b57fb1
Samih Santos De Oliveira <sdsamih@gmail.com>
2024-07-18 02:58:55 -0400

    Update hashing.py
    Transformei a função de iteração de imagem em um gerador.
    Agora ele pode ser reutilizado tanto para a futura função de codificação quanto para a de decodificação
    (E adicionei o tratamento de colisão, quando cair em uma coordenada ocupada, tenta inserir na próxima)

--- hashing.py
+++ hashing.py
@@ -1,4 +1,6 @@
 import cv2
+import numpy as np
+
 from comparar import *
 
 import hashlib
@@ -17,30 +19,55 @@ def textToHash(text):
 
 
 
-def iterate(channel,senha): #receber o canal isolado
+def iterate(caminho,senha): #iterar uma imagem baseado em uma senha, gerador de coordenadas que vai ser usado tanto para codificar quanto para decodificar
+
+    channel = bestRGBChannel(caminho)[1]
+    print(channel)
+
 
     rows=len(channel)
     columns = len(channel[0])
+
+    occupiedMatrix = np.zeros([rows,columns], dtype="bool")
+
 
     positions= rows*columns
 
     hash_atual = textToHash(senha)
 
-    for _ in range(10): #só um numero aleatorio, a verdadeira quantidade é a quantidade de bits da mensagem
+    while (True): #só um numero aleatorio, a verdadeira quantidade é a quantidade de bits da mensagem
         # Convertendo parte do hash para um número inteiro (exemplo: os primeiros 8 bytes)
-        numero_gerado = int.from_bytes(hash_atual[:8], byteorder='big') % positions
+        numero_original=int.from_bytes(hash_atual[8:16], byteorder='big')
+        numero_gerado = (numero_original) % positions
 
         # Atualizando o hash para a próxima iteração
         hash_atual = textToHash(hash_atual)
 
+        chosenrow = numero_gerado // columns
+        chosencolumn = numero_gerado % columns
 
-        chosenrow = numero_gerado//columns
+        while(occupiedMatrix[chosenrow][chosencolumn] == True): #tratamento de colisao (proxima posicao disponivel)
 
-        chosencolumn= numero_gerado %columns
+            numero_gerado= (numero_gerado+1)%positions
 
-        print(f"\ncoordernada escolhida: {chosenrow};{chosencolumn} baseada no numero {numero_gerado} ")
+            chosenrow = numero_gerado // columns
+            chosencolumn = numero_gerado % columns
 
 
-#(bestRGBChannel("imagens/redbest.png")[1],"teste")
+        occupiedMatrix[chosenrow][chosencolumn]=True
 
-print(iterate(bestRGBChannel("imagens/redbest.png")[1],"teste"))
+
+       #print(f"\ncoordernada escolhida: {chosenrow};{chosencolumn} baseada no numero {numero_gerado} ")
+        yield((chosenrow,chosencolumn))
+
+coordGen = iterate("lenna.png","testefdsa")
+
+print(next(coordGen)) # gerador de coordenada (usado para codificar e decodificar)
+print(next(coordGen))
+print(next(coordGen))
+print(next(coordGen))
+print(next(coordGen))
+print(next(coordGen))
+print(next(coordGen))
+print(next(coordGen))
+print(next(coordGen))
